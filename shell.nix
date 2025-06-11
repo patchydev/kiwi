4d2dcf2ba76a1f72d5226f7d20f9a6843b7cbd4e
Isabelle <patchydev@proton.me>
2025-06-11 02:33:42 +0000

    clean up nix shell, hopefully this doesn't break stuff

--- shell.nix
+++ shell.nix
@@ -4,17 +4,11 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    llvm_18
     llvm_18.dev
     zig
-    pkg-config
-    cmake
   ];
 
   shellHook = ''
-    LLVM_CONFIG=${pkgs.llvm_18.dev}/bin/llvm-config
-    LLVM_SYS_180_PREFIX=${pkgs.llvm_18.dev}
-    PKG_CONFIG_PATH="${pkgs.llvm_18.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
     SHELL=${pkgs.zsh}/bin/zsh
     zsh
   '';
